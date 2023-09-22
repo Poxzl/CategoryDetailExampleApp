4630313044ee14f1122916a8077097667e4eeec2
ArjunC <84456473+Poxzl@users.noreply.github.com>
2023-09-22 12:54:44 -0500

    added parcel(bugs present)

--- app/src/main/java/com/example/categorydetailexampleapp/Food.java
+++ app/src/main/java/com/example/categorydetailexampleapp/Food.java
@@ -1,20 +1,90 @@
 package com.example.categorydetailexampleapp;
 
-public class Food {
+import android.media.Image;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Food implements Parcelable {
     private String name;
     private double price;
-    private String type;
+    private String desc;
+    private int imageResourceID = 0;
 
-    public Food(String name, double price, String type) {
+    // A default constructor is required for the Parceable interface to work
+    public Food() {
+        name = "";
+        price = 0;
+        desc = "";
+        imageResourceID = 0;
     }
 
-    public double getPrice() {
-        return price;
+    public String toString() {
+        return this.name;
     }
-    public String getName(){
-        return name;
+
+    /** This is a "constructor" of sorts that is needed with the Parceable interface to
+     * tell the intent how to create a Food object when it is received from the intent
+     * basically it is setting each instance variable as a String or Int
+     * if the instance variables were objects themselves you would need to do more complex * code.  We need to read in the String, double, and int data.
+     *
+     * @param parcel    the parcel that is received from the intent
+     */
+
+    public Food(Parcel parcel) {
+        name = parcel.readString();
+        price = parcel.readDouble();
+        desc = parcel.readString();
+        imageResourceID = parcel.readInt();
     }
-    public String getType(){
-        return type;
+
+    /**
+     * This is what is used when we send the Food object through an intent
+     * It is also a method that is part of the Parceable interface and is needed
+     * to set up the object that is being sent.  Then, when it is received, the
+     * other Food constructor that accepts a Parcel reference can "unpack it"
+     *
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeDouble(price);
+        dest.writeString(desc);
+        dest.writeInt(imageResourceID);
     }
+
+    // this code is needed for the Food class to work with Parcelable
+    public static final Parcelable.Creator<Food> CREATOR = new
+            Parcelable.Creator<Food>() {
+
+                @Override
+                public Food createFromParcel(Parcel parcel) {
+                    return new Food(parcel);
+                }
+
+                @Override
+                public Food[] newArray(int size) {
+                    return new Food[0];
+                }
+            };
+
+    /**
+     * This method is required for the Parceable interface.  As of now, this method * is in the default state and doesn't really do anything.
+     *
+     * If your Parcelable class will have child classes, you'll need to
+     * take some extra care with the describeContents() method. This would
+     * let you identify the specific child class that should be created by
+     * the Parcelable.Creator. You can read more about how this works on
+     *  Stack Overflow with this link.
+     *           https://stackoverflow.com/questions/4778834/purpose-of-describecontents-of-parcelable-interface
+     * @return
+     */
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+
 }
--- app/src/main/java/com/example/categorydetailexampleapp/MainActivity.java
+++ app/src/main/java/com/example/categorydetailexampleapp/MainActivity.java
@@ -7,8 +7,9 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     public static final Food[] myBreakfast = {
-            new Food("Pancakes", 6.99, "4 pancakes"),
-            new Food("Waffles", 7.50, "Crispy Golden Brown")
+            new Food("Pancakes", 6.99, "4 pancakes", R.drawable.pancakes),
+            new Food("Waffles", 7.50, "Crispy Golden Brown", R.drawable.waffles)
+
     };
 
     @Override
